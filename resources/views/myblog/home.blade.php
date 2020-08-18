db586339a3c03635973d235de328b0775cb598c4
Abdi maualana <63388851+AbdiMaulana1802@users.noreply.github.com>
2020-08-18 08:03:38 +0700

    Tugas 1 laravel View dan Routing
    Tugas 1 laravel View dan Routing

---
+++ resources/views/myblog/home.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Blog</title>
+
+</head>
+<body>
+
+   <style>
+        .jumbotron{
+          background: url(img/santiago.jpg)center center;}
+        
+      .display-5{
+        color: white;
+        font-size: 30px;
+        font-family: 'Roboto', sans-serif;
+        color: white;
+      }
+      .navbar-brand{
+          color: white;
+          font-size: 20px;
+          font-family: Roboto;
+      }
+      .container{
+          font-family: Roboto;
+          color: white;
+      }
+      .navbar-nav{
+          font-family: Roboto;
+          font-weight: bold;
+          color: white;
+      }
+     
+     
+    </style>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav text-white" style="font-size: 20px;"> 
+  <a class="nav-item nav-link" href="home">HOME <span class="sr-only">(current)</span></a>
+    <a class="nav-item nav-link" href="#profil">PROFIL</a>
+      <a class="nav-item nav-link" href="experience">EXPERIENCE</a>
+        <a class="nav-item nav-link" href="portofolio">PORTOFOLIO</a>
+      </div>
+    </div>
+</nav>
+
+  <div class="jumbotron">
+    <center>
+      <img src="img/profil.jpeg" width="200px">
+      <h1></h1>
+        <h2><b>(Siswa / Student)</b></h2>
+      <p class="display-5">
+          ABDI MAULANA
+      </p>
+       <h2 class="display-4"><b>Sekolah</b></h2>
+  <p class="display-5">SMK Taruna Bhakti Depok</p>
+    </center>
+  </div>
+
+
+ <div class="container">
+ <div class="row">
+    <div class="col text-center">
+      <h1 id="profil" style="font-family: Roboto">PROFILE</h1><br>
+    </div>
+  </div>
+    <p class="tulisan">
+    Assalamualaikum wr.wb <br>
+    Perkenalkan Nama saya Abdi maulana, atau biasa dipanggil abdi. Saya lahir di depok, 18 febuary 2003 dan saat ini saya berumur 17 tahun.
+    Saat ini, saya bersekolah  di SMK Taruna Bhakti Depok, di SMK Taruna Bhakti Depok Saya mendapatkan Jurusan RPL (Rekayasa perangkat Lunak) dan saya sekarang berada di tingkat atau kelas 12. dan berikut adalah data diri lengkap saya:
+    <br><br>
+
+<!-- Biodata -->
+      <table class="table table-bordered" >
+    <tr>
+        <td>Nama Lengkap:</td>
+        <td> ABDI MAULANA</td>
+    </tr>
+    <tr>
+        <td> Kelas :</td>
+        <td> XII RPL 1</td>
+    </tr>
+    <tr>
+        <td> Sekolah : </td>
+        <td>SMK Taruna Bhakti</td>
+    </tr>
+    <tr>
+        <td>Tempat, Tanggal Lahir : </td>
+        <td>Depok, 18 febuary 2003</td>
+    </tr>
+    <tr>
+        <td>Alamat :</td>
+        <td> Jl.kapitan gg.bhakti 1 Rt 07/01 no.57, Kel. Sukatani, Kec.Tapos, Kota Depok</td>
+    </tr>
+    <tr>
+        <td>Agama : </td>
+        <td>Islam</td>
+    </tr>
+    <tr>
+        <td>Jenis Kelamin : </td>
+        <td>laki-laki</td>
+    </tr>
+    <tr>
+        <td>No Telpon : </td>
+        <td>089638349246</td>
+    </tr>
+    <tr>
+        <td>Email : </td>
+        <td>Abdimaulana1602@gmail.com</td>
+    </tr>
+</table>
+    </p>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ storage/framework/views/bce7c242185675872dfb3666cfb4c4c32811a10a.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <title>Blog</title>
+</head>
+<body>
+
+    <style>
+        .jumbotron{
+          background: url(img/santiago.jpg)center center;
+      }
+      .display-5{
+        color: white;
+        font-size: 20px;
+        font-family: 'Roboto', sans-serif;
+        color: white;
+      }
+      .navbar-brand{
+          color: white;
+          font-size: 20px;
+          font-family: Roboto;
+      }
+      .container{
+          font-family: Roboto;
+          color: white;
+      }
+      .navbar-nav{
+          font-family: Roboto;
+          font-weight: bold;
+          color: white;
+      }
+      h1{
+        color: white;
+      }
+    </style>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <a class="navbar-brand" href="#">ABDI</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav" style="font-size: 20px;"> 
+    <a class="nav-item nav-link" href="home">HOME <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="home">PROFIL</a>
+        <a class="nav-item nav-link" href="experience">EXPERIENCE</a>
+    <a class="nav-item nav-link" href="portofolio">PORTOFOLIO</a>
+</div>
+    </div>
+</nav>
+    
+<div class="jumbotron">
+
+<div class="container">
+    <div class="col text-center">
+        <h1>EXPERIENCE</h1>
+        <br>
+  <div class="row">
+    <div class="col md-3">
+        <div class="card">
+            <img src="img/tutor.png" class="card-img-top">
+                <div class="card-body"><h3><h3>Membuat Aplikasi Mobile yaitu Aplikasi belajar mengaji Android</h3>
+                        <p class="card-text">Pada saat kelas 10 kami pada waktu itu belajar membuat aplikasi belajar mengaji Android, kami diajarkan oleh para kakak tingkat kami mengenai pembuatan aplikasi berbasis mobile dengan menggunakan Android Studio</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col md-3">
+            <div class="card">
+                <img src="img/kunjin.jpg" class="card-img-top">
+                    <div class="card-body"><h3>Kunjungan Industri Ke PT.Indonesia Comnets plus (Icon+)</h3>
+                    <p class="card-text">pada saat kelas 10 kami melaksanakan Kunjungan Industri ke PT Comnets plus (Icon+) bertempat Jl. Damai No.100, Gandul, Kec. Cinere, Kota Depok, Jawa Barat 16514</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+</div>
+    </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/vendor/js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\installaravel\installaravel\resources\views/myblog/experience.blade.php ENDPATH**/ ?>
